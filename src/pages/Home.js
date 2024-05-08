f22a7f006de1c255668fbfa264c98f676b20b26b
Estefanía Saldarriaga Muñoz <128753905+EstefaSM@users.noreply.github.com>
2024-05-07 22:17:27 -0500

    Se soluciona conflicto en el componente Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
 import Header from '../components/Header';
 import Nav from '../components/Nav';
 import Main from '../components/Main';
 import Footer from '../components/Footer';
 import AsideRight from '../components/AsideRight';
 import Dashboard from './Dashboard';
+
+
+
+
+
 
 const Home = () => {
   return (
